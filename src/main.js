e9ac6eb4d7a8de045517db4031d6e0fe3ece0069
larayap <114194469+larayap@users.noreply.github.com>
2025-06-08 20:36:32 -0400

    change version

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
 import { createPinia }   from 'pinia'
 import App from './App.vue'
+import SoundPlugin from '@/plugins/sound'
 
 const app = createApp(App)
 app.use(createPinia())
+app.use(SoundPlugin)
 app.mount('#app')
